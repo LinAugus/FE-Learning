9c66768432f30b1265fd94e0003177f37f3fa20d
LinAugus <linaugus@gmail.com>
2016-08-26 13:52:03 +0800

    zepto学习

---
+++ zepto/hello-appjs/server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var request = require('request');
+var app = new express();
+
+app.use(express.static('public'));
+
+app.get('/',function(req, res, next){
+	res.json(req.headers);
+});
+
+var apiBaseUrl = 'http://news-at.zhihu.com';
+app.get(/^\/api\/.+$/,function (req, res, next){
+	console.log('proxy:{%s => %s}', req.url, apiBaseUrl+ req.url);
+	request.get(apiBaseUrl + req.url, function(err, response, body){
+		if(err){
+			console.log('proxy err:',err);
+			res.json({});
+		}else{
+			res.send(body);
+		}
+
+	});
+});
+
+// 图片防盗链的反代方法
+app.get('/img/proxy',function(req, res, next){
+	if(!req.query.img){
+		res.status(400);
+		res.send('Bad Request');
+	}
+	request.get({url: req.query.img, headers:{ referrer: 'http://news-at.zhihu.com/'}}).pipe(res);
+});
+
+app.listen(3000, function(){
+	console.log('app.js + zepto app ready at port 3000');
+});
+
+module.exports = app;
